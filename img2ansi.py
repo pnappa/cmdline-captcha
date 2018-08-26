d47c87bb5df6f390727374c88dc96d7e1321e812
Patrick Nappa <patricknappa@gmail.com>
2018-08-26 16:03:50 +1000

    added 256 colour mode, works well i think

--- img2ansi.py
+++ img2ansi.py
@@ -2,11 +2,55 @@ import math
 import subprocess
 from PIL import Image
 import sys
+import functools
+
 
 # a (messy) python port of https://github.com/dom111/image-to-ansi, converting it into a nice cmd program
 # TODO: do the unicode thing
 
-def _toAnsi(img, oWidth=40, is_unicode=False):
+# convert a colour to one that can be displayed in a shitty terminal (sorry guys)
+def _rgb_to_256(r,g,b):
+    r,g,b = map(int, (r,g,b))
+    # i actually don't know what this does
+    colours = []
+    colours.append([0, 0, 0, 0])
+    colours.append([128, 0, 0, 1])
+    colours.append([0, 128, 0, 2])
+    colours.append([128, 128, 0, 3])
+    colours.append([0, 0, 128, 4])
+    colours.append([128, 0, 128, 5])
+    colours.append([0, 128, 128, 6])
+    colours.append([192, 192, 192, 7])
+    colours.append([128, 128, 128, 8])
+    colours.append([255, 0, 0, 9])
+    colours.append([0, 255, 0, 10])
+    colours.append([255, 255, 0, 11])
+    colours.append([0, 0, 255, 12])
+    colours.append([255, 0, 255, 13])
+    colours.append([0, 255, 255, 14])
+    colours.append([255, 255, 255, 15])
+
+    for r1 in [0, 95, 135, 175, 215, 255]:
+        for g1 in [0, 95, 135, 175, 215, 255]:
+            for b1 in [0, 95, 135, 175, 215, 255]:
+                colours.append([r1,g1,b1, 16 + int(str(math.floor(5 * r1/255)) + 
+                                            str(math.floor(5 * g1/255)) + 
+                                            str(math.floor(5 * b1/255)),6)])
+
+    for s in [8, 18, 28, 38, 48, 58, 68, 78, 88, 98, 108, 118, 128, 138, 148, 158, 168, 178, 188, 198, 208, 218, 228, 238]:
+        colours.append([s,s,s, 232 + math.floor(s/10)])
+
+    # get the best term colour
+    def best(candidates, source):
+        # sort based on the distance from the populated colour table - closest wins!
+        return sorted(candidates, key=lambda x: abs(x[0] - source[0]) + abs(x[1] - source[1]) + abs(x[2] - source[2]))[0]
+
+    return best(colours, [r,g,b])[3]
+
+def _pix_to_escape(r,g,b,is_256, is_unicode):
+    pass 
+
+def _toAnsi(img, oWidth=40, is_unicode=False, is_256=False):
     destWidth = img.width
     destHeight = img.height
     # produce a scale if the image is too big
@@ -25,23 +69,33 @@ def _toAnsi(img, oWidth=40, is_unicode=False):
     print(img.width, img.height)
     while i < n:
         r,g,b = map(str, img.getpixel((i % img.width, i//img.width)))
-        if is_unicode:
-            #TODO: this is a bit more complicated..
-            pass
+        if is_256:
+            if is_unicode:
+                pass
+            else:
+                ansi_string += '\x1B[48;5;' + str(_rgb_to_256(r,g,b)) + 'm  '
+                i += 1
+                if i % destWidth == 0:
+                    ansi_string += '\x1B[0m\n'
         else:
-            ansi_string += '\x1B[48;2;' + r + ';' + g + ';' + b + 'm  '
-            i += 1
-            # newline
-            if i % destWidth == 0:
-                ansi_string += '\x1B[0m\n'
+            if is_unicode:
+                #TODO: this is a bit more complicated..
+                pass
+            else:
+                ansi_string += '\x1B[48;2;' + r + ';' + g + ';' + b + 'm  '
+                i += 1
+                # newline
+                if i % destWidth == 0:
+                    ansi_string += '\x1B[0m\n'
     print("num pixels:", i)
 
     return ansi_string
 
-def convert(filename):
+def convert(filename, is_unicode=False, is_256=False):
     im = Image.open(filename)
-    stringo = _toAnsi(im)
+    stringo = _toAnsi(im, is_unicode=is_unicode, is_256=is_256)
     return stringo
 
 if __name__ == "__main__":
-    print(convert(sys.argv[1]))
+    print(_rgb_to_256(28, 228, 18))
+    print(convert(sys.argv[1], is_256=True))
